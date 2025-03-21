5c79efebd3143ae9e54195e03caa480a12bc968d
Epsilon Eridani <tsuuzetsu@gmail.com>
2025-03-21 20:47:40 +0300

    added telegram bot controller

---
+++ KworkNotify.Api/Controllers/TelegramBotController.cs
@@ -0,0 +1,30 @@
+﻿using KworkNotify.Core.Service;
+using KworkNotify.Core.Telegram;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using Serilog;
+
+namespace KworkNotify.Api.Controllers;
+
+[Authorize]
+[Route("tg")]
+[ApiController]
+public class TelegramBotController(MongoContext context) : ControllerBase
+{
+    [HttpGet("users")]
+    public async Task<ActionResult<IEnumerable<TelegramUser>>> GetAllUsers()
+    {
+        try
+        {
+            var users = await context.Users.Find(_ => true).ToListAsync();
+            return Ok(users);
+        }
+        catch (Exception e)
+        {
+            const string error = "An error occured while getting all telegram users";
+            Log.ForContext<TelegramBotController>().Error(e, error);
+            return StatusCode(500, error);
+        }
+    }
+}
