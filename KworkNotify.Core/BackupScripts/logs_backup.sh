ec1101190e326395b3436dee2913b5f45714f8b9
Epsilon Eridani <tsuuzetsu@gmail.com>
2025-03-25 15:58:28 +0300

    log file for the current day is no longer deleted

--- KworkNotify.Core/BackupScripts/logs_backup.sh
+++ KworkNotify.Core/BackupScripts/logs_backup.sh
@@ -10,15 +10,17 @@ fi
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 LOGS_BACKUP_NAME="logs_backup_$TIMESTAMP"
 
+CURRENT_DATE=$(date +"%Y%m%d")
+
 echo "Создание бэкапа логов из $LOGS_DIR..."
 tar -czf "$LOGS_BACKUP_NAME.tar.gz" -C "$LOGS_DIR" .
 
 if [ $? -eq 0 ]; then
     echo "Бэкап логов завершен: $LOGS_BACKUP_NAME.tar.gz"
-    echo "Удаление исходных файлов логов..."
-    rm -rf "$LOGS_DIR"/*
+    echo "Удаление исходных файлов логов, кроме логов за $CURRENT_DATE..."
+    find "$LOGS_DIR" -type f -not -name "*$CURRENT_DATE.log" -exec rm -f {} \;
     if [ $? -eq 0 ]; then
-        echo "Исходные файлы логов успешно удалены"
+        echo "Исходные файлы логов (кроме текущего дня) успешно удалены"
     else
         echo "Ошибка при удалении исходных файлов логов"
         exit 1
